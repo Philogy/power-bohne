48806ca72d1dbc575c96158297c6b8127a3ee749
philogy <philippe@dumo.net>
2022-10-05 19:37:07 -0500

    Add link/ref querrier

--- vib_cli.py
+++ vib_cli.py
@@ -2,7 +2,9 @@ import argparse
 import re
 from collections import namedtuple, defaultdict
 import beancount.loader
-from beancount.data import Transaction
+from toolz import curry
+from beancount.core.data import Transaction
+from beancount.parser import printer
 
 
 def add_meta_query_parser(parser_meta_query):
@@ -18,14 +20,84 @@ def add_meta_query_parser(parser_meta_query):
     parser_meta_query.add_argument('-e', '--show-entry', action='store_true')
 
 
+def match_in_set(expr, s):
+    return s and any(re.search(expr, el) for el in s)
+
+
+def get_match(args):
+    match_tags = args.match_tags
+    match_links = args.match_links
+    match_any = not (match_tags or match_links)
+    return match_tags or match_any, match_links or match_any
+
+
+@curry
+def keep_entry(args, entry):
+    if not isinstance(entry, Transaction):
+        return False
+
+    expr = args.expression
+
+    match_tags, match_links = get_match(args)
+    return (match_tags and match_in_set(expr, entry.tags))\
+        or (match_links and match_in_set(expr, entry.links))
+
+
+def get_tags(entries, expr):
+    for entry in entries:
+        for tag in entry.tags:
+            if re.search(expr, tag):
+                yield tag
+
+
+def get_links(entries, expr):
+    for i, entry in enumerate(entries, start=1):
+        for link in entry.links:
+            if re.search(expr, link):
+                yield f'{link} ({i})'
+
+
 def meta_query(args):
     entries, errors, options_map = beancount.loader.load_file(args.filepath)
     if errors:
         raise errors[0]
 
+    match_tags, match_links = get_match(args)
+    entries = list(filter(keep_entry(args), entries))
+
+    if match_tags:
+        print('Tags:')
+        for tag in get_tags(entries, args.expression):
+            print(tag)
+
+    if match_links:
+        if match_tags:
+            print()
+        print('Links:')
+        for link in get_links(entries, args.expression):
+            print(link)
+
+    # show added info
+    print('\nEntries:')
     for entry in entries:
-        if not isinstance(entry, Transaction):
-            continue
+        if args.show_entry:
+            printer.print_entry(entry)
+        else:
+            res_str = ''
+            if args.show_date:
+                res_str += entry.date.strftime('%Y-%m-%d')
+            if args.show_payee:
+                res_str += f' "{entry.payee}"'
+            if args.show_narration:
+                res_str += f' "{entry.narration}"'
+            if args.show_tags:
+                res_str += ' ' + ' '.join(map(lambda t: f'#{t}', entry.tags))
+            if args.show_links:
+                res_str += ' ' + ' '.join(map(lambda t: f'^{t}', entry.links))
+            if args.show_tags or args.show_links:
+                res_str += '\n'
+
+            print(res_str)
 
 
 Command = namedtuple('Command', ['parser_add', 'command_fn'])
@@ -36,7 +108,7 @@ COMMAND_ALIASES = {
 }
 
 COMMANDS = {
-    'metaquery': Command(add_meta_query_parser, None)
+    'metaquery': Command(add_meta_query_parser, meta_query)
 }
 
 
