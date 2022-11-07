869a1628725952deae1a3cae3f38291e517bcdbf
philogy <philippe@dumo.net>
2022-11-07 21:24:27 +0800

    Make package

---
+++ README.md
@@ -0,0 +1,3 @@
+# Power Bohne
+A set of importers and plugins for beancount to facilitate the booking of
+Cryptocurrency and DeFi related transactions.
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+
+setup(
+    # metadata
+    name='power_bohne',
+    version='0.0.1',
+    description='A set of plugins, importers and scripts to ease crypto accounting with beancount',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.10'
+    ],
+    author='Philogy',
+    url='https://github.com/Philogy/power_bohne',
+    # actual data
+    entry_points={
+        'console_scripts': ['vib-bohne = vib_cli:main']
+    },
+    install_requires=['toolz', 'beancount >= 2.0.0']
+)
