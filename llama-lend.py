140ec3713e7a1542081bfcb98f8c444fda9feea8
Philogy <philippe@dumo.net>
2023-04-05 13:30:14 +0200

    ✨ Add llama lend full E2E tx extraction

--- llama-lend.py
+++ llama-lend.py
@@ -4,6 +4,10 @@ import dotenv
 import requests
 from collections import defaultdict
 from decimal import Decimal as D
+from datetime import datetime
+from dateutil import tz
+from power_bohne.prices import coingecko
+from argparse import ArgumentParser
 
 dotenv.load_dotenv()
 
@@ -35,17 +39,49 @@ def get_alchmey_asset_transfers(key, **params):
     ).json()
 
 
+def get_block_timestamp(key, block_num):
+    res = requests.post(
+        f'https://eth-mainnet.g.alchemy.com/v2/{key}',
+        headers=JSON_HEADER,
+        json={
+            'id': 1,
+            'jsonrpc': '2.0',
+            'method': 'eth_getBlockByNumber',
+            'params': [
+                hex(block_num),
+                False
+            ]
+        }
+    ).json()
+    return hex_to_int(res['result']['timestamp'])
+
+
 def hex_to_int(h):
     return int(h[2:], 16)
 
 
+def blocks_from_transfers(transfers) -> set:
+    return {
+        transfer['blockNum']
+        for transfer in transfers
+    }
+
+
+def unix_to_local(timestamp: int):
+    local_tz = tz.tzlocal()
+    local_dt = datetime.fromtimestamp(timestamp, local_tz)
+    return local_dt
+
+
 def get_asset_transfers(pool):
     alchemy_key = os.environ.get('ALCHEMY_KEY')
+    print('Retrieving outgoing transfers...')
     transfers_out = get_alchmey_asset_transfers(
         alchemy_key,
         category=['external', 'internal', 'erc721'],
         fromAddress=pool
     )
+    print('Retrieving incoming transfers...')
     transfers_in = get_alchmey_asset_transfers(
         alchemy_key,
         category=['external', 'internal', 'erc721'],
@@ -68,8 +104,23 @@ def get_asset_transfers(pool):
         transfers_in['result']['transfers']
     ]
 
+    blocks = blocks_from_transfers(cleaned_transfers)
+    print('Retrieving block timestamps...')
+    block_to_dates = {
+        block: unix_to_local(get_block_timestamp(alchemy_key, block))
+        for block in blocks
+    }
+
+    dated_transfers = [
+        {
+            **transfer,
+            'date': block_to_dates[transfer['blockNum']]
+        }
+        for transfer in cleaned_transfers
+    ]
+
     grouped_transfers = defaultdict(list)
-    for transfer in cleaned_transfers:
+    for transfer in dated_transfers:
         grouped_transfers[transfer['hash']].append(transfer)
 
     sorted_transfers = sorted(
@@ -96,7 +147,7 @@ def etherscan(tx_hash):
     return f'https://etherscan.io/tx/{tx_hash}'
 
 
-def format_bean_repay(tx_hash, borrow_txs, interest):
+def format_bean_repay(tx_hash, borrow_txs, interest, date, price):
     if len(borrow_txs) == 1:
         borrow_tx, = borrow_txs
         borrow_formatted = f'''  borrow_tx: "{borrow_tx}"
@@ -109,31 +160,75 @@ def format_bean_repay(tx_hash, borrow_txs, interest):
   borrow_tx-{i}-link: "{etherscan(borrow_tx)}"
 '''
 
-    return f'''2023-MM-DD * "Llama Lend" "Gobbler loan repayment + interest" #eth
+    return f'''{date.strftime("%Y-%m-%d")} * "Llama Lend" "Gobbler loan repayment + interest" #eth
   txref: "{tx_hash}"
   txref-link: "{etherscan(tx_hash)}"
 {borrow_formatted}  Income:Crypto:Lending
-  Assets:Crypto:Tokens      {D(interest) / D(10**18)} ETH {{}}
+  Assets:Crypto:Tokens      {D(interest) / D(10**18)} ETH {{{round(price, 2)} EUR}}
 '''
 
 
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument('pool', type=str)
+    parser.add_argument('-b', '--beancount', action='store_true')
+    parser.add_argument('-t', '--from-tx', default=None)
+    return parser.parse_args()
+
+
 def main():
-    pool, *args = sys.argv[1:]
-    txs = get_asset_transfers(pool)
+    args = parse_args()
+    pool = args.pool
+    all_txs = get_asset_transfers(pool)
     pool = pool.lower()
 
-    beancount = args and args[0] == '-b'
+    beancount = args.beancount
     beancounts = []
+
+    skip_tx = args.from_tx
+    skipping = skip_tx is not None
+    skipped = 0
 
     borrows = {}
 
+    txs = []
+    for tx_hash, tx_transfers in all_txs:
+        if skipping:
+            skipped += 1
+            if tx_hash == skip_tx:
+                skipping = False
+            continue
+        txs.append((tx_hash, tx_transfers))
+
+    date_start = txs[0][1][0]['date']
+    date_end = txs[-1][1][0]['date']
+
+    print('Retrieving coingecko prices...')
+    prices = coingecko.get_price_over_range(
+        'ethereum',
+        'eur',
+        date_start.timestamp(),
+        date_end.timestamp()
+    )
+
+    def get_price(dt):
+        price, _, _ = coingecko.get_lin_avg_price(
+            prices, datetime.fromtimestamp(dt.timestamp()))
+        return price
+
     for tx_hash, tx_transfers in txs:
+
         prints = [
             None,
             f'    tx hash: {tx_hash}',
             f'    link: {etherscan(tx_hash)}'
         ]
         tx_type = None
+        if len(tx_transfers) == 0:
+            raise ValueError(f'Unrecognized: {tx_transfers}')
+
+        date = tx_transfers[0]['date']
+
         if len(tx_transfers) == 1 and is_eth((transfer := tx_transfers[0])['asset']):
             if transfer['from'] == pool:
                 tx_type = 'WITHDRAWAL'
@@ -164,7 +259,6 @@ def main():
                 f'    change: {sign(pool_eth_gain)}{pool_eth_gain/ 1e18:,.6f} ETH'
             )
             if pool_eth_gain > 0 and not tokens_in:
-                print(f'tokens_out: {tokens_out}')
                 tx_type = 'REPAY'
                 total_principal = 0
                 borrow_txs = set()
@@ -182,9 +276,13 @@ def main():
                     for borrow_tx in borrow_txs:
                         prints.append(f'     - {etherscan(borrow_tx)}')
                 if beancount:
-                    beancounts.append(
-                        format_bean_repay(tx_hash, borrow_txs, interest)
-                    )
+                    beancounts.append(format_bean_repay(
+                        tx_hash,
+                        borrow_txs,
+                        interest,
+                        date,
+                        get_price(date)
+                    ))
 
             elif pool_eth_gain < 0 and not tokens_out:
                 tx_type = 'BORROW'
@@ -214,7 +312,7 @@ def main():
 
         else:
             raise ValueError(f'Unrecognized: {tx_transfers}')
-        prints[0] = f'tx: {tx_hash[:8]}..{tx_hash[-6:]} [{tx_type}]'
+        prints[0] = f'tx: {tx_hash[:8]}..{tx_hash[-6:]} [{tx_type}] ({date.strftime("%Y-%m-%d %H:%M")})'
 
         for p in prints:
             print(p)
@@ -227,7 +325,9 @@ def main():
         for b in beancounts:
             print(b)
 
-    print(f'borrows: {borrows}')
+    print(f'skipped {skipped} / {len(all_txs)}')
+    if skipping:
+        print(f'Warning: skip tx {skip_tx} not found in txs')
 
 
 if __name__ == '__main__':
