6190c356bd689e7887245b3e57d6b55e80106f47
Philippe Dumonet <philippe@dumo.net>
2023-12-13 12:33:48 -0500

    ✨ Add metadata enumeration

--- power_bohne/vib_cli/metaquery.py
+++ power_bohne/vib_cli/metaquery.py
@@ -1,4 +1,6 @@
 import re
+from argparse import ArgumentParser
+from typing import Generator
 import beancount.loader
 from beancount.parser import printer
 from beancount.core.data import Transaction
@@ -10,11 +12,29 @@ def match_in_set(expr, s):
     return s and any(re.search(expr, el) for el in s)
 
 
-def get_match(args):
-    match_tags = args.match_tags
-    match_links = args.match_links
-    match_any = not (match_tags or match_links)
-    return match_tags or match_any, match_links or match_any
+def get_match(args) -> tuple[bool, ...]:
+    match_tags: bool = args.match_tags
+    match_links: bool = args.match_links
+    match_meta: bool = args.match_metadata
+    matching = (match_tags, match_links, match_meta)
+    match_any = not any(matching)
+    return tuple(map(lambda m: m or match_any, matching))
+
+
+def get_valid_meta_fields(expr: str, meta: dict[str, str]) -> list[tuple[str, str]]:
+    assert ':' in expr, f'Expression does not match <field regex>:<regex>'
+    field_expr, val_expr = expr.split(':', 1)
+    return [
+        (field, value)
+        for field, value in meta.items()
+        if isinstance(value, str)
+        and re.search(field_expr, field)
+        and re.search(val_expr, value)
+    ]
+
+
+def matches_meta(expr: str, meta: dict[str, str]) -> bool:
+    return bool(get_valid_meta_fields(expr, meta))
 
 
 @curry
@@ -24,9 +44,10 @@ def keep_entry(args, entry):
 
     expr = args.expression
 
-    match_tags, match_links = get_match(args)
+    match_tags, match_links, match_meta = get_match(args)
     return (match_tags and match_in_set(expr, entry.tags))\
-        or (match_links and match_in_set(expr, entry.links))
+        or (match_links and match_in_set(expr, entry.links))\
+        or (match_meta and matches_meta(expr, entry.meta))
 
 
 def get_tags(entries, expr):
@@ -43,25 +64,47 @@ def get_links(entries, expr):
                 yield f'{link} ({i})'
 
 
+def get_metas(entries, expr: str) -> Generator[str, None, None]:
+    field_expr, val_expr = expr.split(':', 1)
+    for entry in entries:
+        for field, value in entry.meta.items():
+            if re.search(field_expr, field) and re.search(val_expr, value):
+                yield value
+
+
 def meta_query(args):
     entries, errors, _ = beancount.loader.load_file(args.filepath)
     if errors:
         raise errors[0]
 
-    match_tags, match_links = get_match(args)
+    match_tags, match_links, match_meta = get_match(args)
     entries = list(filter(keep_entry(args), entries))
 
+    prev = False
+
     if match_tags:
+        if prev:
+            print()
         print('Tags:')
         for tag in get_tags(entries, args.expression):
             print(tag)
+        prev = True
 
     if match_links:
-        if match_tags:
+        if prev:
             print()
         print('Links:')
         for link in get_links(entries, args.expression):
             print(link)
+        prev = True
+
+    if match_meta:
+        if prev:
+            print()
+        print('Metadata:')
+        for meta in get_metas(entries, args.expression):
+            print(meta)
+        prev = True
 
     # show added info
     print('\nEntries:')
@@ -80,23 +123,34 @@ def meta_query(args):
                 res_str += ' ' + ' '.join(map(lambda t: f'#{t}', entry.tags))
             if args.show_links:
                 res_str += ' ' + ' '.join(map(lambda t: f'^{t}', entry.links))
-            if args.show_tags or args.show_links:
-                res_str += '\n'
+            if args.show_meta_value:
+                fields = get_valid_meta_fields(args.expression, entry.meta)
+                if len(fields) == 1:
+                    field, value = fields[0]
+                    res_str += f' {field}: {value}'
+                elif len(fields) > 1:
+                    for field, value in fields:
+                        res_str += f'\n    {field}: {value}'
 
-            print(res_str)
+            # if args.show_tags or args.show_links:
+            #     res_str += '\n'
+
+            if res_str:
+                print(res_str)
 
 
-def add_meta_query_parser(parser):
+def add_meta_query_parser(parser: ArgumentParser):
     parser.add_argument('filepath')
     parser.add_argument('expression', type=str)
+    parser.add_argument('-m', '--match-metadata', action='store_true')
     parser.add_argument('-t', '--match-tags', action='store_true')
     parser.add_argument('-l', '--match-links', action='store_true')
     parser.add_argument('-a', '--show-tags', action='store_true')
     parser.add_argument('-i', '--show-links', action='store_true')
     parser.add_argument('-d', '--show-date', action='store_true')
     parser.add_argument('-p', '--show-payee', action='store_true')
-    parser.add_argument('-n', '--show-narration',
-                        action='store_true')
+    parser.add_argument('-n', '--show-narration', action='store_true')
+    parser.add_argument('-v', '--show-meta-value', action='store_true')
     parser.add_argument('-e', '--show-entry', action='store_true')
 
 
